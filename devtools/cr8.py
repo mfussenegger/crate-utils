cdcd3844c8858bd288fc353ad7bdded1fdf7fdf5
Mathias Fussenegger <f.mathias@zignar.net>
2015-08-04 19:27:22 +0200

    add blob upload script

--- devtools/cr8.py
+++ devtools/cr8.py
@@ -4,13 +4,14 @@
 
 from timeit import timeit
 from json2insert import json2insert
+from blobs import upload
 import argh
 
 
 
 def main():
     p = argh.ArghParser()
-    p.add_commands([timeit, json2insert])
+    p.add_commands([timeit, json2insert, upload])
     p.dispatch()
 
 
